723d5e9629d5b76841b3becc7905d1f126bd58d7
Wespinoz <wallyarriola@gmail.com>
2023-06-04 10:11:13 -0600

    Validacion-ingreso de login-Wally

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IconsService } from '@shared/services/icons.service';
 import { AuthService } from '../../services/auth.service';
+import { AlertService } from '@shared/services/alert.service';
 
 @Component({
   selector: 'vex-login',
@@ -28,34 +29,42 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  constructor(private fb: FormBuilder, private authService: AuthService,
-    private router: Router, private cd: ChangeDetectorRef) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private cd: ChangeDetectorRef,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit(): void {
     this.initForm();
   }
 
   login(): void {
-    if(this.form.invalid) {
-      return Object.values(this.form.controls).forEach((controls) => {
+    if (this.form.invalid) {
+      Object.values(this.form.controls).forEach((controls) => {
         controls.markAllAsTouched();
       });
+      this.alertService.warn('Atención', 'Contraseña incorrecta. Por favor, verifícala.');
+      return;
     }
 
     this.authService.login(this.form.value).subscribe((resp) => {
-      if(resp.isSuccess) {
-        this.router.navigate(["/"]);
+      if (resp.isSuccess) {
+        this.alertService.success('Inicio de sesión Exitoso', '');
+        this.router.navigate(['/']);
       }
     });
   }
 
   toggleVisibility(): void {
     if (this.visible) {
-      this.inputType = "password";
+      this.inputType = 'password';
       this.visible = false;
       this.cd.markForCheck();
     } else {
-      this.inputType = "text";
+      this.inputType = 'text';
       this.visible = true;
       this.cd.markForCheck();
     }
@@ -63,6 +72,7 @@ export class LoginComponent implements OnInit {
 
   forgotPassword(): void {
     this.forgotPasswordClicked = true;
-    this.forgotPasswordMessage = 'Para poder Restablecer tu Password debes comunicarte al correo de soporte nominaumg@gmail.com';
+    this.forgotPasswordMessage =
+      'Para poder Restablecer tu Password debes comunicarte al correo de soporte nominaumg@gmail.com';
   }
 }
